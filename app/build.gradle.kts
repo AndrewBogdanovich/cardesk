83665d290495b82b935595d122b311d362de5ef5
AndrewBohdanovich <andrew.bogdanovich@cogniteq.com>
2023-09-17 23:18:18 +0300

    upd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -50,9 +50,9 @@ dependencies {
     //recyclerview
     implementation("androidx.recyclerview:recyclerview:1.3.1")
     //lifecycle
-    implementation("androidx.lifecycle:lifecycle-common:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-common:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     //retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:3.12.0")
@@ -68,8 +68,8 @@ dependencies {
     //room
     implementation("androidx.room:room-runtime:2.5.2")
     //navigation component
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.1")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
     //appcenter
     implementation("com.microsoft.appcenter:appcenter-analytics:5.0.2")
     implementation("com.microsoft.appcenter:appcenter-crashes:5.0.2")
--- app/src/main/java/com/example/cardesk/presentation/advertisement/MyAdvertisementFragment.kt
+++ app/src/main/java/com/example/cardesk/presentation/advertisement/MyAdvertisementFragment.kt
@@ -1,15 +1,15 @@
-package com.example.cardesk.presentation.ad
+package com.example.cardesk.presentation.advertisement
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.cardesk.databinding.FragmentMyAdsBinding
+import com.example.cardesk.databinding.FragmentMyAdvertisementBinding
 import com.example.cardesk.presentation.setupToolbar
 
-class MyAdsFragment : Fragment() {
-    private var _binding: FragmentMyAdsBinding? = null
+class MyAdvertisementFragment : Fragment() {
+    private var _binding: FragmentMyAdvertisementBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -17,7 +17,7 @@ class MyAdsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentMyAdsBinding.inflate(inflater, container, false)
+        _binding = FragmentMyAdvertisementBinding.inflate(inflater, container, false)
         setupToolbar(isShowing = true, title = "My ads")
         return binding.root
     }
