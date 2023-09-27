4ecdae922f3066578346933cd9a4e3a8ada7eefe
AndrewBohdanovich <htconev228@gmail.com>
2023-09-28 00:35:30 +0300

    upd inputTypes for passwords fields

--- app/src/main/java/com/example/cardesk/presentation/authorization/LoginFragment.kt
+++ app/src/main/java/com/example/cardesk/presentation/authorization/LoginFragment.kt
@@ -1,17 +1,21 @@
 package com.example.cardesk.presentation.authorization
 
 import android.os.Bundle
+import android.text.InputType
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.cardesk.R
 import com.example.cardesk.databinding.FragmentLoginBinding
 import com.example.cardesk.presentation.navigateTo
 
-class LoginFragment: Fragment() {
+class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+    private var isShowingPassword = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +26,19 @@ class LoginFragment: Fragment() {
         binding.forgotPasswordTv.setOnClickListener {
             toForgotPasswordScreen()
         }
+        binding.loginPasswordLayout.setEndIconOnClickListener {
+            if (!isShowingPassword) {
+                binding.loginPasswordEt.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                isShowingPassword = true
+            } else {
+                binding.loginPasswordEt.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                isShowingPassword = false
+            }
+        }
         return binding.root
     }
 
-    private fun toForgotPasswordScreen(){
+    private fun toForgotPasswordScreen() {
         navigateTo(R.id.action_fragment_authorization_to_forgotPasswordFragment)
     }
 
--- app/src/main/java/com/example/cardesk/presentation/authorization/RegisterFragment.kt
+++ app/src/main/java/com/example/cardesk/presentation/authorization/RegisterFragment.kt
@@ -1,15 +1,17 @@
 package com.example.cardesk.presentation.authorization
 
 import android.os.Bundle
+import android.text.InputType
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.cardesk.databinding.FragmentRegisterBinding
 
-class RegisterFragment: Fragment() {
+class RegisterFragment : Fragment() {
     private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
+    private var isShowingPassword = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,6 +19,15 @@ class RegisterFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        binding.registerPasswordLayout.setEndIconOnClickListener {
+            if (!isShowingPassword) {
+                binding.registerPasswordEt.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                isShowingPassword = true
+            } else {
+                binding.registerPasswordEt.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                isShowingPassword = false
+            }
+        }
         return binding.root
     }
 
